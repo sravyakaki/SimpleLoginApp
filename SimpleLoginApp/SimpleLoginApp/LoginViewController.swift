e3f4cc9de76d00e50d91cf682d3fd8bd8c7d43cc
Sravya Kaki <sravyakaki92@gmail.com>
2020-12-23 21:38:51 -0800

    Login View Controller UI

---
+++ SimpleLoginApp/SimpleLoginApp/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleLoginApp
+//
+//  Created by Sravya Kaki on 12/23/20.
+//  Copyright © 2020 Sravya Kaki. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleLoginApp/SimpleLoginApp/Model/LoginCredentials.swift
@@ -0,0 +1,14 @@
+//
+//  LoginCredentials.swift
+//  SimpleLoginApp
+//
+//  Created by Sravya Kaki on 12/23/20.
+//  Copyright © 2020 Sravya Kaki. All rights reserved.
+//
+
+import Foundation
+
+struct LoginCredentials {
+    var username: String = ""
+    var password: String = ""
+}
