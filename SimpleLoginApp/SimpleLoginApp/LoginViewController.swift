6278dc0d8eae0f6cb0d42bb595a7e9444e7b77b9
Sravya Kaki <sravyakaki92@gmail.com>
2020-12-23 21:46:27 -0800

    Login View controller changes

--- SimpleLoginApp/SimpleLoginApp/LoginViewController.swift
+++ SimpleLoginApp/SimpleLoginApp/LoginViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+ //MARK:- Outlet connections
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
